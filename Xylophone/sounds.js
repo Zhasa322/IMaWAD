a6588e47bcc71bc9fdc7e75c9e51a0b4e5293c8f
Zhasa322 <46893833+Zhasa322@users.noreply.github.com>
2024-10-18 18:29:37 +0500

    Add files via upload

---
+++ Xylophone/sounds.js
@@ -0,0 +1,30 @@
+function soundred(){
+ const audio = new Audio(`A.wav`);
+ audio.play()
+
+}
+
+function soundorange(){
+    const audio = new Audio(`B.wav`);
+    audio.play()
+}
+
+function soundyellow(){
+    const audio = new Audio(`C.wav`);
+    audio.play()
+}
+
+function soundgreen(){
+    const audio = new Audio(`D.wav`);
+    audio.play()
+}
+
+function soundblue(){
+    const audio = new Audio(`E.wav`);
+    audio.play()
+}
+
+function soundpurple(){
+    const audio = new Audio(`F.wav`);
+    audio.play()
+}
